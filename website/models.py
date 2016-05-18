12e922b353cccf0b374f28906cc8dce9fcf59457
ZZJMAC <hljzzj@qq.com>
2016-05-18 23:51:19 +0800

    Home

--- website/models.py
+++ website/models.py
@@ -6,3 +6,17 @@ from django.db import models
 class UserInfo(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
+    WorkGroup = models.CharField(max_length=50)
+    CreateDate = models.DateTimeField()
+    Logintime = models.DateTimeField()
+    LoginIP = models.CharField(max_length=16)
+    Super = models.CharField(max_length=10)
+
+
+class IPInfo(models.Model):
+
+
+class WorkGroup(models.Model):
+    ID = models.IntegerField():
+    WorkGroup = models.CharField(max_length=50)
+
